b7f7e95ce4e94d8b214353a09036bd1d59191e68
gonpuga <gonpuga@gmail.com>
2020-04-20 16:51:34 +0200

    Updating GUI with View.post()

--- app/src/main/java/com/example/colorswitch/MainActivity.java
+++ app/src/main/java/com/example/colorswitch/MainActivity.java
@@ -2,15 +2,16 @@ package com.example.colorswitch;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG=MainActivity.class.getName();
+    private static final String TAG = MainActivity.class.getName();
     private LinearLayout screenLayout = null;
     public EditText resultField;
 
@@ -18,22 +19,23 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        screenLayout = (LinearLayout) findViewById(R.id.layoutMain);
-        resultField = (EditText) findViewById(R.id.resultField);
+        screenLayout = findViewById(R.id.layoutMain);
+        resultField = findViewById(R.id.resultField);
         Log.v(TAG, "En onCreate():" + Thread.currentThread().getId());
     }
 
     public void redOnClick(View v) {
-        Log.v(TAG, "En redOnClick():"+Thread.currentThread().getId());
+        Log.v(TAG, "En redOnClick():" + Thread.currentThread().getId());
         screenLayout.setBackgroundColor(Color.RED);
     }
+
     public void greenOnClick(View v) {
-        Log.v(TAG, "En greenOnClick():"+Thread.currentThread().getId());
+        Log.v(TAG, "En greenOnClick():" + Thread.currentThread().getId());
         screenLayout.setBackgroundColor(Color.GREEN);
     }
 
     public void blueOnClick(View v) {
-        Log.v(TAG, "En blueOnClick():"+Thread.currentThread().getId());
+        Log.v(TAG, "En blueOnClick():" + Thread.currentThread().getId());
         screenLayout.setBackgroundColor(Color.BLUE);
     }
 
@@ -44,9 +46,10 @@ public class MainActivity extends AppCompatActivity {
                         Thread.currentThread().getId());
                 try {
                     Thread.currentThread().sleep(4000);
-                } catch (InterruptedException e) { }
+                } catch (InterruptedException e) {
+                }
 
-                resultField.post(new Runnable(){
+                resultField.post(new Runnable() {
                     @Override
                     public void run() {
                         resultField.setText("Resultado " +
@@ -57,4 +60,3 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 }
-
